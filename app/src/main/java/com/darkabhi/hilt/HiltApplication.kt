3b57b7b5dab42088350f0f0f137f8e3d444f5ac8
Abhishek AN <darkabhi1520@gmail.com>
2021-03-15 03:51:47 +0530

    Field injection
    Signed-off-by: Abhishek AN <darkabhi1520@gmail.com>

---
+++ app/src/main/java/com/darkabhi/hilt/HiltApplication.kt
@@ -0,0 +1,12 @@
+package com.darkabhi.hilt
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Abhishek AN <abhishek@iku.earth> on 3/15/2021.
+ */
+
+@HiltAndroidApp
+class HiltApplication:Application() {
+}
--- app/src/main/java/com/darkabhi/hilt/MainActivity.kt
+++ app/src/main/java/com/darkabhi/hilt/MainActivity.kt
@@ -2,10 +2,26 @@ package com.darkabhi.hilt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    // Field injection
+    @Inject
+    lateinit var someClass: SomeClass
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println(someClass.doAThing())
+    }
+}
+
+
+class SomeClass @Inject constructor() {
+    fun doAThing():String {
+        return "I DID IT!"
     }
 }
