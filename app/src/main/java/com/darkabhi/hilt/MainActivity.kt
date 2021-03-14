706a7706d03c1609ca93e23ce28cda845c87d157
Abhishek AN <darkabhi1520@gmail.com>
2021-03-15 03:54:47 +0530

    Constructor injection
    Signed-off-by: Abhishek AN <darkabhi1520@gmail.com>

--- app/src/main/java/com/darkabhi/hilt/MainActivity.kt
+++ app/src/main/java/com/darkabhi/hilt/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.darkabhi.hilt
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -16,12 +16,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println(someClass.doAThing())
+        println(someClass.doSomeOtherThing())
     }
 }
 
 
-class SomeClass @Inject constructor() {
-    fun doAThing():String {
+class SomeClass @Inject constructor(private val someOtherClass: SomeOtherClass) {
+    fun doAThing(): String {
         return "I DID IT!"
+    }
+
+    fun doSomeOtherThing(): String {
+        return someOtherClass.doSomeOtherThing()
+    }
+}
+
+class SomeOtherClass @Inject constructor() {
+    fun doSomeOtherThing(): String {
+        return "I DID THE OTHER THING!"
     }
 }
