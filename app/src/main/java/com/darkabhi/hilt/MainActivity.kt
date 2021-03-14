96a31fb129b865359ee71046cb7fb3098a667cc7
Abhishek AN <darkabhi1520@gmail.com>
2021-03-15 04:31:00 +0530

    Bind annotation build interface impl
    Signed-off-by: Abhishek AN <darkabhi1520@gmail.com>

--- app/src/main/java/com/darkabhi/hilt/MainActivity.kt
+++ app/src/main/java/com/darkabhi/hilt/MainActivity.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.gson.Gson
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.internal.managers.ApplicationComponentManager
 import dagger.hilt.android.scopes.ActivityScoped
 import dagger.hilt.android.scopes.FragmentScoped
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -24,7 +29,7 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-class SomeClass @Inject constructor(private val someInterfaceImpl: SomeInterface, private val gson: Gson) {
+class SomeClass @Inject constructor(private val someInterfaceImpl: SomeInterface) {
     fun doAThing(): String {
         return "I GOT IT ${someInterfaceImpl.getAThing()}"
     }
@@ -38,4 +43,12 @@ class SomeInterfaceImpl @Inject constructor():SomeInterface {
 
 interface SomeInterface{
     fun getAThing():String
+}
+
+@InstallIn(SingletonComponent::class)
+@Module
+abstract class MyModule{
+    @Singleton
+    @Binds
+    abstract fun bindSomeDependency(someImpl:SomeInterfaceImpl) :SomeInterface
 }
